04bafbdf4dd43cbfd6d24417b56db74daa9832d0
Edison Ade <ask@buzzedison.com>
2019-05-27 21:52:00 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <HelloWorld msg="OSIKAN WEBSITE COMING SOON"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 export default {
   name: 'app',
   components: {
@@ -23,6 +24,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  background-color: yellow;
+  padding-top: 20px;
+  padding-bottom:20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/css/style.css";
+import "./assets/img/bg-masthead.jpg";
+import "./assets/img/bg-signup.jpg";
+import "./assets/img/demo-image-01.jpg";
+import "./assets/img/demo-image-02.jpg";
+import "./assets/img/ipad.png";
+import "./assets/scss/scale.scss";
+import "./assets/scss/about.scss";
+import "./assets/scss/_buttons.scss";
+import "./assets/scss/navbar.scss";
+import "./assets/scss/_projects.scss";
+import "./assets/scss/masthead.scss";
+import "./assets/scss/_contact.scss";
+import "./assets/scss/_signup.scss";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
